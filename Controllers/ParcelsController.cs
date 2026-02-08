78c2379697f804a423cc104feb1ff33f1d2e2c76
Thad Weldon <thadw29@gmail.com>
2026-02-08 10:43:38 -0500

    feat: simple index and details views for seeing parcels and its data

---
+++ Controllers/ParcelsController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LandParcelFinder.Data;
+using LandParcelFinder.Models;
+
+namespace LandParcelFinder.Controllers
+{
+    public class ParcelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ParcelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: Parcels request
+        public async Task<IActionResult> Index()
+        {
+            var parcels = await _context.Parcels.ToListAsync();
+            return View(parcels);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var parcel = await _context.Parcels.FirstOrDefaultAsync(m => m.ParcelId == id);
+
+            if(parcel == null)
+            {
+                return NotFound();
+            }
+
+            return View(parcel);
+        }
+
+        // GET: Parcels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ParcelNumber,Address,County,SizeInAcres,Price,HasWetlands,Status,Notes")] Parcel parcel)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(parcel);
+                await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Parcel added!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(parcel);
+        }
+    }
+}
